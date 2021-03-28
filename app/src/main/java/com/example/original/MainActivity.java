9270432a2d8d11e66ca1f5670d621b0302cd2b07
Tomohiro-Kawakami-techcamp <zhikuanchuanshang725@gmail.com>
2021-03-28 21:22:26 +0900

    途中まで

--- app/src/main/java/com/example/original/MainActivity.java
+++ app/src/main/java/com/example/original/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,5 +22,25 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.menu_light, menu);
         return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsItemSelected(MenuItem item) {
+        View view = findViewById(R.id.activity_main);
+        switch (item.getItemId()){
+            case R.id.red:
+                view.setBackgroundColor(Color.RED);
+                return true;
+            case R.id.green:
+                view.setBackgroundColor(Color.GREEN);
+                return true;
+            case R.id.blue:
+                view.setBackgroundColor(Color.BLUE);
+                return true;
+            case R.id.white:
+                view.setBackgroundColor(Color.WHITE);
+                return true;
+        }
+        return false;
     }
 }

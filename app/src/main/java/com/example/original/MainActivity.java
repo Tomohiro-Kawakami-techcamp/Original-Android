416e402aa065b7a061ce10ec09b194cf7e389b3e
Tomohiro-Kawakami-techcamp <zhikuanchuanshang725@gmail.com>
2021-03-28 20:55:39 +0900

    編集

--- app/src/main/java/com/example/original/MainActivity.java
+++ app/src/main/java/com/example/original/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.original;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        View view = findViewById(R.id.activity_main);
+        view.setBackgroundColor(Color.WHITE);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_light, menu);
+        return true;
     }
 }
